0a9f8dc66b9df23d3e48f62142c56b9a30c3abda
Arpit Agarwal <agarwalarpit22@gmail.com>
2015-12-15 16:36:33 +0530

    path fix

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/about', function (req, res) {
 	res.send("We are learning nodeJS!");
 });
 
-app.use(express.static(__dirname + "\\public"));
+app.use(express.static(__dirname + "/public"));
 
 app.listen(port, function () {
 	console.log('Express Server startted on port: ' + port);
